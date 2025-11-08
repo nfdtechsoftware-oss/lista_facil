fbfd631139f7967a2d11bab9f815421fe62b2827
NFDTech <nfdtechsoftware@gmail.com>
2025-11-08 14:44:07 -0300

    chore: configurar metadados do Android
    - Atualizar nome do app para 'Lista Fácil'
    - Configurar applicationId: com.nfdtech.listafacil
    - Definir compileSdk 36, minSdk 21, targetSdk 34
    - Ativar minify e shrink para release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.nfdtech.listafacil.lista_facil"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.nfdtech.listafacil"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,20 +20,22 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.nfdtech.listafacil.lista_facil"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "com.nfdtech.listafacil"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            // TODO: Configure keystore for release signing
             signingConfig = signingConfigs.getByName("debug")
         }
     }
